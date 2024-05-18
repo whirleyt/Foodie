9cb5f8103300f1c7ff40bbd3a8d96013480b6b51
Tara Whirley <whirleyt@outlook.com>
2024-05-18 14:13:09 -0400

    first commit

---
+++ backend/src/main/java/com/example/mybackend/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.example.mybackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MyController {
+
+    @GetMapping("/data")
+    public String getData() {
+        return "Hello from Java backend!";
+    }
+}
